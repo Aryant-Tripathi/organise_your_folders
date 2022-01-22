4145b868c04f5d0d493ad4511d9c2f577a905b7d
Aryant-Tripathi <60316716+Aryant-Tripathi@users.noreply.github.com>
2022-01-22 15:42:45 +0530

    Update organize.js

--- commands/organize.js
+++ commands/organize.js
@@ -1,5 +1,13 @@
 let fs = require("fs");
 let path = require("path");
+let types = {
+    image:["jpg","png","jpeg","gif"],
+    media: ["mp4", "mkv","mp3"],
+    archives: ["zip", "7z", "rar", "tar", "gz", "ar", "iso", "xz"],
+    documents: ["docx", "doc", "pdf", "xlsx", "xls", "odt", "ods", "odp", "odg", "odf", "txt", "ps", "tex"],
+    app: ["exe", "dmg", "pkg", "deb"]
+}
+
 function organizeFn(dirPath)
 {
     //node main.js organise "directorypath "
